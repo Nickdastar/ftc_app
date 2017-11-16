4acd91cd117240067b62c3f014b7ea04819f2cbb
NICKS-LAPTOP\nicks <nickster241@gmail.com>
2017-11-16 07:49:27 -0800

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomustest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomustest.java
@@ -1,8 +1,72 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Nikolas on 11/15/2017.
  */
 
-public class RoboSquadTeleOp {
+@Autonomous(name = "TestAutonomous")
+public class Autonomustest extends LinearOpMode
+{
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private DcMotor motorArm;
+
+    private Servo servoLeft;
+    private Servo servoRight;
+
+    private static final double RELIC_GRAB_POSITION = 0.8;
+    private static final double RELIC_RELEASE_POSITION = 0.2;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        motorLeft = hardwareMap.dcMotor.get("motorLeft" );
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorArm = hardwareMap.dcMotor.get("motorArm");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        servoLeft = hardwareMap.servo.get("servoLeft");
+        servoRight = hardwareMap.servo.get("servoRight");
+
+        servoRight.setPosition(RELIC_GRAB_POSITION);
+        servoLeft.setPosition(RELIC_GRAB_POSITION);
+
+        waitForStart();
+
+        // Drive Forward
+        DriveForward(DRIVE_POWER);
+        Thread.sleep(4000);
+        //stop
+       StopDriving();
+    }
+    double DRIVE_POWER = 1.0;
+
+        public void DriveForward(double power)
+        {
+            motorLeft.setPower(power);
+            motorRight.setPower(power);
+        }
+
+        public void TurnLeft(double power)
+        {
+            motorLeft.setPower(-power);
+            motorRight.setPower(power);
+        }
+
+        public void StopDriving()
+        {
+            DriveForward(0);
+        }
+
+        public void TurnRight(double power)
+        {
+            motorLeft.setPower(power);
+            motorRight.setPower(-power);
+        }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboSquadTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboSquadTeleOp.java
@@ -1,8 +1,64 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Nikolas on 11/15/2017.
  */
 
-public class RoboSquadTeleOp {
+@TeleOp(name = "RobosquadsTeleOp")
+public class RoboSquadTeleOp extends LinearOpMode
+{
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private DcMotor motorTreads;
+
+    private Servo servoLeft;
+    private Servo servoRight;
+
+    private static final double RELIC_GRAB_POSITION = 0.8;
+    private static final double RELIC_RELEASE_POSITION = 0.2;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorTreads = hardwareMap.dcMotor.get("motorArm");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        servoLeft = hardwareMap.servo.get("servoLeft");
+        servoRight = hardwareMap.servo.get("servoRight");
+
+        servoRight.setPosition(RELIC_GRAB_POSITION);
+        servoLeft.setPosition(RELIC_GRAB_POSITION);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            motorLeft.setPower(-gamepad1.left_stick_y);
+            motorRight.setPower(-gamepad1.right_stick_y);
+            motorTreads.setPower(-gamepad2.right_stick_y);
+
+            if (gamepad2.right_bumper) {
+                servoLeft.setPosition(RELIC_GRAB_POSITION);
+            }
+
+            if (gamepad2.left_bumper) {
+                servoLeft.setPosition(RELIC_RELEASE_POSITION);
+            }
+
+            if (gamepad2.right_trigger) {
+                servoRight.setPosition(RELIC_GRAB_POSITION);
+            }
+
+            if (gamepad2.left_trigger) {
+                servoRight.setPosition(RELIC_RELEASE_POSITION);
+            }
+        }
+    }
 }
